8d534e81acc1f1ee82e0bc724f3986d94a59eed6
seovee <jinseob102@naver.com>
2023-11-10 18:19:17 +0900

    [Design] 404페이지 스타일 적용

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,28 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+
 export default function NotFound() {
-  return "404에러 페이지입니다.";
+  return (
+    <section id="project">
+      <div className="h-[100vh] flex flex-col gap-5 justify-center items-center">
+        <h1 className="hover:font-bold cursor-pointer">
+          <motion.p
+            initial={{ display: "none", opacity: 0 }}
+            animate={{ display: "inline", opacity: 1 }}
+            className="text-6xl font-bold"
+          >
+            {`404 오류 페이지입니다. 🥹`}
+          </motion.p>
+        </h1>
+        <p className="text-5xl font-light">주소를 확인해주세요.</p>
+
+        <Link
+          href="/"
+          className="border bg-gray-300 px-5 py-3 rounded-2xl mt-10"
+        >
+          👈🏻 Home
+        </Link>
+      </div>
+    </section>
+  );
 }
