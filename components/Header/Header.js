0694be84e38b5381ef9d50a7dd6cb0c5441e670e
seovee <jinseob102@naver.com>
2023-11-10 17:53:09 +0900

    [Design] Header Blur 효과 및 배치 적용

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,23 +2,35 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="relative">
-      <nav className="top-[10vh] w-full flex items-center justify-center h-20 bg-pink-100">
-        <ul className="flex gap-10 text-xl font-normal ">
+    // 불투명한 배너 사용(backdrop)
+    <header className="fixed top-0 left-0 right-0 z-10 flex flex-row backdrop-blur-md">
+      <h1 className="absolute flex items-center justify-center left-0 w-36 h-20 text-2xl font-bold bg-gray-500/10">
+        <Link href="#">Logo!</Link>
+      </h1>
+      <nav className="w-full h-20 flex items-center justify-center">
+        <ul className="flex gap-20 text-xl font-normal">
           <li>
-            <Link href="#">Home</Link>
+            <Link href="#" className="font-semibold">
+              HOME
+            </Link>
           </li>
           <li>
-            <Link href="#about">About</Link>
+            <Link href="#about" className="font-semibold ">
+              ABOUT
+            </Link>
           </li>
           <li>
-            <Link href="#project">Project</Link>
+            <Link href="#project" className="font-semibold">
+              PROJECT
+            </Link>
           </li>
           <li>
-            <Link href="#contact">Contact</Link>
+            <Link href="#contact" className="font-semibold">
+              CONTACT
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   );
 }
