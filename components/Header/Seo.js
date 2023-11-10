d524db36767a085315983ce0aeb36966fea35df0
seovee <jinseob102@naver.com>
2023-11-10 18:29:17 +0900

    [Feat] Seo 적용(Head 기능사용)

---
+++ components/Header/Seo.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Seo() {
+  return (
+    <Head>
+      <title>Jinseob portfolio | Part 1</title>
+    </Head>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Seo from "./Header/Seo";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Seo />
       <Header />
       <div>{children}</div>
       <Footer />
