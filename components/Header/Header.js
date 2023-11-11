acee92a14c68de8ce716c1ec961d97d06ce477ef
seovee <jinseob102@naver.com>
2023-11-11 22:01:33 +0900

    [Feat] Header 애니메이션 추가

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     // 불투명한 배너 사용(backdrop)
-    <header className="fixed top-0 left-0 right-0 z-10 flex flex-row backdrop-blur-md">
+    <motion.header
+      className="fixed top-0 left-0 right-0 z-10 flex flex-row backdrop-blur-md"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 2 } }}
+    >
       <h1 className="absolute flex items-center justify-center left-0 w-36 h-20 text-2xl font-bold bg-gray-500/10">
         <Link href="/" className="italic">
           Logo!
@@ -33,6 +38,6 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 }
