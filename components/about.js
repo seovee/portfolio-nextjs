7f1587e5aed64bca9762bb573746f8d9ab56d341
seovee <jinseob102@naver.com>
2023-11-12 00:58:19 +0900

    [Feat] about 페이지 스타일 + 캐러셀 버튼 추가

--- components/about.js
+++ components/about.js
@@ -1,6 +1,7 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Profile from "../public/profile.jpeg";
+import { aboutData } from "../data/aboutData";
 
 export default function About() {
   const { scrollYProgress } = useScroll();
@@ -11,12 +12,14 @@ export default function About() {
   );
 
   return (
-    <section id="about">
+    <section id="about" className="flex flex-col justify-center items-center">
       <motion.div
-        className="relative h-[100vh] flex justify-evenly items-center"
+        className="relative h-[100vh] flex justify-around items-center"
         style={{ scrollYProgress, backgroundColor }}
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
       >
-        <h2 className="absolute top-0 font-gmarket font-bold">ABOUT</h2>
+        <h2 className="absolute top-10 font-thin">- A B O U T -</h2>
         <div>
           <Image
             src={Profile}
@@ -26,10 +29,57 @@ export default function About() {
             className="rounded-full"
           />
         </div>
-        <div>
-          <ul>
-            <li>Data를 map으로 써서 넣어보자!!!!!!!!!!!!!!!!!!!</li>
+        <div className="w-1/2">
+          <ul className="cursor-default">
+            {aboutData.map((data, index) => (
+              <li key={index}>
+                <p className="font-jaram text-4xl mt-10 mb-1 font-medium hover:font-black">
+                  {data.title}
+                </p>
+                <p className="font-thin text-xl">{data.contetnt}</p>
+              </li>
+            ))}
           </ul>
+        </div>
+        {/* Button 컴포넌트 */}
+        <div className="absolute bottom-10">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{
+              opacity: [0, 1, 0],
+              scale: [0, 1.5, 0],
+              transition: { delay: 1.2, duration: 1.5 },
+            }}
+            className="bg-blue-500 w-20 h-20 rounded-full"
+          ></motion.div>
+        </div>
+        <div className="absolute bottom-10">
+          <motion.div
+            animate={{
+              width: [0, 80, 80, 240],
+              height: [0, 80, 80, 80],
+              borderRadius: [0, 80, 80, 80],
+              transition: { delay: 1, duration: 1.5 },
+            }}
+            className="bg-gray-200 flex flex-row justify-evenly items-center"
+          >
+            <motion.div
+              animate={{ scale: [0, 1], transition: { delay: 2.5 } }}
+              className="w-4 h-4 rounded-full bg-black cursor-pointer"
+            ></motion.div>
+            <motion.div
+              animate={{ scale: [0, 1], transition: { delay: 2.5 } }}
+              className="w-4 h-4 rounded-full bg-black cursor-pointer"
+            ></motion.div>
+            <motion.div
+              animate={{ scale: [0, 1], transition: { delay: 2.5 } }}
+              className="w-4 h-4 rounded-full bg-black cursor-pointer"
+            ></motion.div>
+            <motion.div
+              animate={{ scale: [0, 1], transition: { delay: 2.5 } }}
+              className="w-4 h-4 rounded-full bg-black cursor-pointer"
+            ></motion.div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
