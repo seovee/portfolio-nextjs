844ff9cecf8231a9aefc02c5c1694c0ddb9dfe24
seovee <jinseob102@naver.com>
2023-11-11 23:12:16 +0900

    [Feat] about페이지에 들어갈 세부 Data

---
+++ data/aboutData.js
@@ -0,0 +1,7 @@
+//* 소개페이지에 들어갈 Data
+
+export const aboutData = [
+  { title: "타이틀 제목", contetnt: "소개 세부내용" },
+  { title: "타이틀 제목2", contetnt: "소개 세부내용2" },
+  { title: "타이틀 제목3", contetnt: "소개 세부내용3" },
+];
