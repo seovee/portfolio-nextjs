384e400de5c87b2196940b7140a24e95390022a7
seovee <jinseob102@naver.com>
2023-11-14 23:45:39 +0900

    [Design] Contact, Footer 스타일 적용

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return "footer 들어갈 자리입니다.";
+  return (
+    <footer className="h-20">
+      <div className="flex justify-center items-end pb-5 text-white text-xs bg-emerald-400 w-full h-full">
+        Copyright 2023. JinSeob Kim. All rights reserved.
+      </div>
+    </footer>
+  );
 }
--- components/contact.js
+++ components/contact.js
@@ -2,17 +2,19 @@ import { motion } from "framer-motion";
 
 export default function Contact() {
   return (
-    <section id="contact">
-      <div className="bg-lime-300 h-[100vh] flex justify-center items-center">
-        <h1 className="text-8xl hover:font-bold cursor-pointer">
-          <motion.p
-            initial={{ display: "none", opacity: 0 }}
-            animate={{ display: "inline", opacity: 1 }}
-            className="text-5xl font-bold md:text-9xl"
-          >
-            {`Contact 페이지 입니다 🥹`}
-          </motion.p>
-        </h1>
+    <section
+      id="contact"
+      className="relative flex flex-col justify-center items-center"
+    >
+      <div className="bg-gradient-to-b from-lime-300 to-emerald-400 w-full h-[100vh] flex justify-center items-center">
+        <h2 className="absolute top-10 font-thin">- C O N T A C T -</h2>
+        <motion.p
+          initial={{ display: "none", opacity: 0 }}
+          animate={{ display: "inline", opacity: 1 }}
+          className="text-5xl font-bold md:text-9xl"
+        >
+          {`Contact 페이지 입니다 🥹`}
+        </motion.p>
       </div>
     </section>
   );
