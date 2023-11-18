0134a55caac3ab43ac5c25dac568b88cb390093b
seovee <jinseob102@naver.com>
2023-11-18 23:46:43 +0900

    [Feat] contact 페이지 기초 구조 생성

--- components/contact.js
+++ components/contact.js
@@ -4,17 +4,19 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="relative flex flex-col justify-center items-center"
+      className="relative flex flex-row justify-around items-center bg-gradient-to-b from-lime-300 to-emerald-400 w-full h-[100vh]"
     >
-      <div className="bg-gradient-to-b from-lime-300 to-emerald-400 w-full h-[100vh] flex justify-center items-center">
-        <h2 className="absolute top-10 font-thin">- C O N T A C T -</h2>
-        <motion.p
-          initial={{ display: "none", opacity: 0 }}
-          animate={{ display: "inline", opacity: 1 }}
-          className="text-5xl font-bold md:text-9xl"
-        >
-          {`Contact 페이지 입니다 🥹`}
-        </motion.p>
+      <h2 className="absolute top-10 font-thin">- C O N T A C T -</h2>
+      <div>
+        <h3 className="font-gmarket text-9xl">CONTACT ME🫡</h3>
+        <ul className="flex gap-10 bg-blue-500/50">
+          <li>이메일</li>
+          <li>깃허브</li>
+          <li>블로그</li>
+        </ul>
+      </div>
+      <div className="bg-gray-500/50 w-1/4 h-1/4">
+        애니메이션 이미지 추가공간
       </div>
     </section>
   );
