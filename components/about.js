f352bcddcedf7e5635c1c2a4ef365c2bcbea3113
seovee <jinseob102@naver.com>
2023-11-19 23:32:14 +0900

    [Feat] 소개페이지 overlay 기본 틀 생성

--- components/about.js
+++ components/about.js
@@ -5,11 +5,16 @@ import {
   useTransform,
 } from "framer-motion";
 import Image from "next/image";
-import Profile from "../public/assets/profile/profile2.jpg";
 import { aboutData } from "../data/index";
 import { useState } from "react";
 import Button from "./Button/button";
 import { useRouter } from "next/router";
+import Profile from "../public/assets/profile/profile.jpeg";
+import Profile2 from "../public/assets/profile/profile2.jpg";
+import Profile3 from "../public/assets/profile/profile3.jpg";
+import Profile4 from "../public/assets/profile/profile4.jpg";
+import Profile5 from "../public/assets/profile/profile5.jpg";
+import Profile6 from "../public/assets/profile/profile6.jpg";
 
 export default function About() {
   const router = useRouter();
@@ -102,7 +107,57 @@ export default function About() {
               exit={{ opacity: 0, y: 10 }}
               className="absolute left-0 right-0 top-[100px] h-[70vh] w-[80vw] mx-auto my-0 rounded-[25px] bg-white "
             >
-              여기를 채우자
+              <section className="flex flex-row p-10 h-full gap-10">
+                <div className="flex flex-row justify-center items-center w-1/2 h-full">
+                  <Image
+                    src={Profile2}
+                    alt="김진섭의 프로필 사진"
+                    width={600}
+                    height={600}
+                    style={{ width: "40%", height: "auto" }}
+                    className="shadow-2xl"
+                  />
+                  <Image
+                    src={Profile3}
+                    alt="김진섭의 프로필 사진"
+                    width={600}
+                    height={600}
+                    style={{ width: "40%", height: "auto" }}
+                    className="shadow-2xl"
+                  />
+                  <Image
+                    src={Profile4}
+                    alt="김진섭의 프로필 사진"
+                    width={600}
+                    height={600}
+                    style={{ width: "40%", height: "auto" }}
+                    className="shadow-2xl"
+                  />
+                  <Image
+                    src={Profile5}
+                    alt="김진섭의 프로필 사진"
+                    width={600}
+                    height={600}
+                    style={{ width: "40%", height: "auto" }}
+                    className="shadow-2xl"
+                  />
+                </div>
+                <div className=" justify-center items-center w-1/2 h-full">
+                  <p>이름 : 김진섭</p>
+                  <p>생년월일 : 1991년 11월 29일</p>
+                  <p>별자리 : 사수자리</p>
+                  <p>MBTI : INFJ</p>
+                  <p>
+                    간단소개 : 체계가 정립되지 않은 공직업무 수행 중 효율적인
+                    방법을 찾으려 노력했습니다. 이경험으로 효율성과 혁신을
+                    추구하는 “개발자”로의 전향을 결심하게 되었습니다. 퇴사 후
+                    프론트엔드 부트캠프에 참여하여 기초를 다지고 스킬을
+                    익혔습니다. 이제는 효율적이고 사용자 친화적인 웹
+                    인터페이스를 구현하는 프론트엔드 개발자로서 성장하고
+                    싶습니다.
+                  </p>
+                </div>
+              </section>
             </motion.div>
           </motion.div>
         ) : null}
