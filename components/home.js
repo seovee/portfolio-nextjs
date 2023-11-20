4ecdf656f83f412324d9d95b9d6a83b7f21500df
seovee <jinseob102@naver.com>
2023-11-20 19:27:54 +0900

    [Design] home 페이지 CSS

--- components/home.js
+++ components/home.js
@@ -1,9 +1,18 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import { FiChevronDown } from "react-icons/fi";
 
 export default function Home() {
   // useScroll을 이용한 title부분 애니메이션 구현
   const { scrollYProgress, scrollY } = useScroll();
+  // Y 비율 확인 함수
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+  });
   const opacity = useTransform(scrollY, [0, 100], [1, 0]);
   const scale = useTransform(scrollY, [0, 500], [1, 3]);
   const backgroundColor = useTransform(
@@ -15,21 +24,21 @@ export default function Home() {
   return (
     <section id="home" className="relative">
       <motion.div
-        className="h-[150vh] flex flex-col gap-[5px] pt-[45vh] justify-start items-center overflow-hidden	"
+        className="h-[150vh] flex flex-col pt-[40vh] justify-start items-center overflow-hidden"
         style={{ scrollYProgress, backgroundColor }}
       >
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { type: "spring" } }}
-          className="text-8xl hover:font-bold cursor-pointer mb-5 font-giants"
+          className="text-9xl hover:font-bold cursor-pointer mb-5 font-giants"
           style={{ scrollY, scale }}
         >
-          {`Hello, Every One`}
+          Hello, Every One
         </motion.h1>
         <motion.h2
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { delay: 0.5 } }}
-          className="text-5xl font-light"
+          className="text-5xl font-light mt-10"
           style={{ scrollY, opacity }}
         >
           프론트엔드 개발자
@@ -37,7 +46,7 @@ export default function Home() {
         <motion.h3
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { delay: 0.5 } }}
-          className="text-4xl font-semibold"
+          className="text-4xl font-semibold mt-2"
           style={{ scrollY, opacity }}
         >
           김진섭 입니다.
