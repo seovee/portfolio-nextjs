0876178fc0bdb4aaaedf75f7e38c90492be927cb
seovee <jinseob102@naver.com>
2023-11-20 16:45:49 +0900

    [Feat] contact 페이지 기초틀 완성

--- components/contact.js
+++ components/contact.js
@@ -1,22 +1,50 @@
-import { motion } from "framer-motion";
+import { RiMailFill } from "react-icons/ri";
+import { SiVelog } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import Iphone from "../public/iphone.png";
+import Image from "next/image";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 export default function Contact() {
+  const { scrollYProgress } = useScroll();
+  const x = useTransform(scrollYProgress, [0.8, 1], [-600, 0]);
   return (
     <section
       id="contact"
-      className="relative flex flex-row justify-around items-center bg-gradient-to-b from-lime-300 to-emerald-400 w-full h-[100vh]"
+      className="relative flex flex-row justify-between items-center bg-gradient-to-b from-lime-300 to-emerald-400 w-full h-[100vh] overflow-hidden"
     >
-      <h2 className="absolute top-10 font-thin">- C O N T A C T -</h2>
-      <div>
-        <h3 className="font-gmarket text-9xl">CONTACT ME🫡</h3>
-        <ul className="flex gap-10 bg-blue-500/50">
-          <li>이메일</li>
-          <li>깃허브</li>
-          <li>블로그</li>
+      <h2 className="absolute top-10 text-center w-full font-thin">
+        - C O N T A C T -
+      </h2>
+      <motion.div
+        className="flex w-1/3 h-full items-center mt-28"
+        style={{ scrollYProgress, x }}
+      >
+        <Image
+          src={Iphone}
+          alt="휴대폰 사진"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </motion.div>
+      <div className="w-2/3">
+        <h3 className="font-gmarket font-bold text-9xl">CONTACT_ME</h3>
+        <ul className="flex mt-10 ml-5 gap-10 items-center">
+          <li>
+            <a href="https://github.com/seovee">
+              <FaGithub size="50" className="hover:text-white" />
+            </a>
+          </li>
+          <a href="https://velog.io/@jinseob102">
+            <li>
+              <SiVelog size="45" className="hover:text-white" />
+            </li>
+          </a>
+          <a href="mailto:jinseob102@naver.com">
+            <li>
+              <RiMailFill size="56" className="hover:text-white" />
+            </li>
+          </a>
         </ul>
-      </div>
-      <div className="bg-gray-500/50 w-1/4 h-1/4">
-        애니메이션 이미지 추가공간
       </div>
     </section>
   );
