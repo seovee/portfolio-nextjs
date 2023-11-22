70e9375713f66b9c90292a44e4d0c57400c03bd9
seovee <jinseob102@naver.com>
2023-11-22 22:29:13 +0900

    [Feat] typeWriter-effect 효과 추가 + CSS 수정 + router 추가

--- components/home.js
+++ components/home.js
@@ -4,9 +4,12 @@ import {
   useScroll,
   useTransform,
 } from "framer-motion";
+import { useRouter } from "next/router";
 import { FiChevronDown } from "react-icons/fi";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
+  const router = useRouter();
   // useScroll을 이용한 title부분 애니메이션 구현
   const { scrollYProgress, scrollY } = useScroll();
   // Y 비율 확인 함수
@@ -20,39 +23,56 @@ export default function Home() {
     [0, 0.1],
     ["rgb(253 224 71)", "rgb(125 211 252)"]
   );
+  const titleClick = () => {
+    router.push("#about");
+  };
 
   return (
-    <section id="home" className="relative">
+    <section id="home" className="relative overflow-hidden select-none">
       <motion.div
-        className="h-[150vh] flex flex-col pt-[40vh] justify-start items-center overflow-hidden"
+        className="h-[150vh] flex flex-col pt-[40vh] justify-start items-center "
         style={{ scrollYProgress, backgroundColor }}
       >
         <motion.h1
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { type: "spring" } }}
-          className="text-9xl hover:font-bold cursor-pointer mb-5 font-giants"
+          initial={{ scale: 3 }}
+          animate={{
+            scale: 1,
+            transition: {
+              type: "spring",
+              bounce: 5,
+              damping: 10,
+              duration: 1,
+            },
+          }}
+          className="text-9xl hover:font-bold mb-5 font-giants"
           style={{ scrollY, scale }}
         >
           Hello, Every One
         </motion.h1>
-        <motion.h2
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { delay: 0.5 } }}
-          className="text-5xl font-light mt-10"
-          style={{ scrollY, opacity }}
-        >
-          프론트엔드 개발자
-        </motion.h2>
-        <motion.h3
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { delay: 0.5 } }}
-          className="text-4xl font-semibold mt-2"
-          style={{ scrollY, opacity }}
-        >
-          김진섭 입니다.
-        </motion.h3>
         <motion.div
-          className="absolute bottom-[50vh] font-light text-3xl"
+          className="flex flex-col items-center"
+          style={{ scrollY, opacity }}
+        >
+          <span className="text-5xl text-center font-light mt-10 tracking-wide">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(1500)
+                  .typeString("공무원?")
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .pauseFor(2000)
+                  .typeString(
+                    `프론트엔드 개발자 <br><span style="font-weight:600;margin-top: 10px;"}>김진섭</span> 입니다.`
+                  )
+                  .start();
+              }}
+              options={{ delay: 100, deleteSpeed: 200 }}
+            />
+          </span>
+        </motion.div>
+        <motion.div
+          className="absolute flex justify-center items-center bottom-[50vh] my-0 font-light text-3xl cursor-pointer w-screen h-20 hover:bg-white/30 "
           animate={{
             opacity: [0, 1, 0],
             transition: {
@@ -60,6 +80,7 @@ export default function Home() {
               repeat: Infinity,
             },
           }}
+          onClick={titleClick}
         >
           <FiChevronDown />
         </motion.div>
