2226e188c3dce58848fee558c0d6f6ee5898fea4
seovee <jinseob102@naver.com>
2023-11-23 23:55:19 +0900

    [Design] overlay 스타일, CSS 수정

--- components/overlay.js
+++ components/overlay.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import BackImg2 from "../public/assets/etc/background1.jpeg";
 import BackImg1 from "../public/assets/etc/background3.jpeg";
-import Profile from "../public/assets/profile/profile4.jpg";
+import Profile from "../public/assets/profile/profile4-1.jpg";
 import { skillData } from "../data";
 
 export default function Overlay({ toggleView }) {
@@ -34,21 +34,45 @@ export default function Overlay({ toggleView }) {
                 alt="첫번째 배경 이미지"
                 width={600}
                 height={600}
-                style={{ width: "auto", height: "100%" }}
+                style={{ width: "100%", height: "auto" }}
                 className="shadow-md"
               />
             </div>
-            <motion.div className="flex items-center justify-center overflow-hidden font-jalnan text-5xl">
-              HTML/CSS, JavaScript
+            <motion.div className="flex flex-col justify-between overflow-hidden font-jalnan text-4xl">
+              <p className="text-amber-400">
+                HTML,
+                <br />
+                CSS,
+                <br />
+                JavaScript
+              </p>
+              <p className="text-right text-gray-400">
+                Git,
+                <br />
+                Github,
+                <br />
+                Notion
+              </p>
             </motion.div>
             <motion.div
-              className="flex items-center justify-center overflow-hidden font-jalnan text-5xl"
+              className="flex items-center justify-center overflow-hidden font-jalnan text-5xl text-sky-300"
               animate={{
                 rotate: 360,
-                transition: { duration: 0.8, repeat: Infinity, repeatDelay: 2 },
+                transition: {
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  repeatDelay: 1,
+                },
               }}
             >
-              React, NextJS, Vue.js
+              React,
+              <br />
+              NextJS,
+              <br />
+              Vue.js,
+              <br />
+              Gatsby
             </motion.div>
             <div className="relative overflow-hidden">
               <Image
@@ -56,12 +80,12 @@ export default function Overlay({ toggleView }) {
                 alt="두번째 배경 이미지"
                 width={600}
                 height={600}
-                style={{ width: "auto", height: "100%" }}
+                style={{ width: "100%", height: "auto" }}
                 className="shadow-md"
               />
             </div>
           </div>
-          <div className="flex flex-col h-full gap-5">
+          <div className="w-1/2 flex flex-col h-full gap-5">
             <div className="h-1/2 overflow-hidden rounded-tr-[25px]">
               <Image
                 src={Profile}
@@ -72,32 +96,27 @@ export default function Overlay({ toggleView }) {
                 className="shadow-md"
               />
             </div>
-            <div className="h-1/2 font-gmarket flex flex-col justify-between pb-2">
-              <h2 className="text-xl font-bold">
+            <div className="h-1/2 font-gmarket flex flex-col justify-between">
+              <h2 className="text-3xl font-bold">
                 김진섭(Kim JinSeob), 1991. 11. 29.
               </h2>
 
               <div>
-                <span>Experience</span>
+                <span className="text-2xl font-semibold">경력 & 교육</span>
                 <ul>
                   <li>
-                    ✓ 2022.11.28. - 2022.30.30. 멋쟁이사자처럼 프론트엔드 4기
+                    ✓ 2022.11.28. - 2023.03.30. 멋쟁이사자처럼 프론트엔드 4기
                     수료
                   </li>
                   <li>✓ 2020.07. - 2022.11. 포천시청 공무원</li>
                   <li>✓ 2010.03. - 2018.02. 강원대학교 환경공학과</li>
                 </ul>
               </div>
-              <div className="flex flex-wrap gap-2 ">
-                {skillData.map((i, index) => (
-                  <span
-                    key={index}
-                    className="inline-block text-xs bg-gray-300 px-2 pt-1 pb-0 rounded-md text-orange-600 font-semibold whitespace-nowrap"
-                  >
-                    {i}
-                  </span>
-                ))}
-              </div>
+              <ul className="flex gap-5 ">
+                <li>이력서 : 링크</li>
+                <li>블로그 : 링크</li>
+                <li>인스타그램 : 링크</li>
+              </ul>
             </div>
           </div>
         </section>
