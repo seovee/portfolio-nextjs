3a860364c2e58ae7c5e23a202c13d8f168bfcab2
seovee <jinseob102@naver.com>
2023-11-24 18:08:19 +0900

    [Feat] project 사진 추가

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 // Image에서 사진은 import해서 보내야 사진이 보여진다.
 import Project_1 from "../public/assets/project/project1.png";
 import Project_2 from "../public/assets/project/project2.png";
-import Project_3 from "../public/assets/project/project3.png";
+import Project_4 from "../public/assets/project/Project4.png";
+import Project_5 from "../public/assets/project/project5.png";
+import Project_6 from "../public/assets/project/project6.png";
 
 // 소개 Data
 export const aboutData = [
@@ -39,8 +41,9 @@ export const projectData = [
   {
     num: 1,
     title: "포트폴리오 웹사이트",
-    category: "NextJS 프로젝트 / 개인프로젝트",
+    category: "개인프로젝트",
     date: "2023. 11. 07. ~ 11. 24.",
+    refacDate: "",
     desc: "첫 포트폴리오 웹페이지입니다. NextJS 만들었고, framer-motion, Gsap을 이용한 애니메이션을 통해 인터렉티브하게 구현해보았습니다.",
     img: Project_1,
     url: "https://portfolio-nextjs-tan-seven.vercel.app/",
@@ -50,8 +53,9 @@ export const projectData = [
   {
     num: 2,
     title: "넷플릭스 클론 사이트",
-    category: "React 프로젝트 / 개인프로젝트",
+    category: "개인프로젝트",
     date: "2023. 11. 07. ~ 11. 24.",
+    refacDate: "2023.11.07. ~ 11. 24",
     desc: "React를 이용한 넷플릭스 클론코딩입니다. 영화API를 불러와 최신, 인기 영화의 정보를 전달합니다.",
     img: Project_2,
     url: "https://seovee.github.io/masterClass-Fin",
@@ -60,46 +64,46 @@ export const projectData = [
   },
   {
     num: 3,
+    title: "티빙 클론 사이트 - Taing",
+    category: "팀프로젝트(5명)",
+    date: "2023. 11. 07. ~ 11. 24.",
+    desc: "리액트, framer-motion, firebase를 이용한 갤러리 웹페이지입니다.",
+    img: Project_2,
+    url: "https://likelion-fe4-rp11.github.io/Asparagus/",
+    github_url: "https://github.com/seovee/masterClass-Fin",
+    skill: ["NextJS", "React", "Framer-motion", "Gsap", "TypeWriter-effect"],
+  },
+  {
+    num: 4,
     title: "I's gallary 갤러리",
-    category: "React 프로젝트 / 팀프로젝트(5명)",
+    category: "팀프로젝트(5명)",
     date: "2023. 11. 07. ~ 11. 24.",
     desc: "리액트, framer-motion, firebase를 이용한 갤러리 웹페이지입니다. 멋쟁이사자처럼 FE스쿨 4기에서 참여했던 프로젝트입니다.",
-    img: Project_3,
+    img: Project_4,
     url: "https://likelion-fe4-rp11.github.io/Asparagus/",
     github_url: "https://github.com/seovee/masterClass-Fin",
     skill: ["React", "Framer-motion", "Firebase", "Figma"],
   },
   {
-    num: 4,
-    title: "티빙 클론 사이트 - Taing",
-    category: "React 프로젝트 / 팀프로젝트(5명)",
-    date: "2023. 11. 07. ~ 11. 24.",
-    desc: "리액트, framer-motion, firebase를 이용한 갤러리 웹페이지입니다.",
-    img: Project_3,
-    url: "https://likelion-fe4-rp11.github.io/Asparagus/",
-    github_url: "https://github.com/seovee/masterClass-Fin",
-    skill: ["NextJS", "React", "Framer-motion", "Gsap", "TypeWriter-effect"],
-  },
-  {
     num: 5,
     title: "회원가입 페이지",
-    category: "React 프로젝트 / 팀프로젝트(5명)",
+    category: "팀프로젝트(5명)",
     date: "2023. 11. 07. ~ 11. 24.",
-    desc: "리액트, framer-motion, firebase를 이용한 갤러리 웹페이지입니다.",
-    img: Project_3,
-    url: "https://likelion-fe4-rp11.github.io/Asparagus/",
-    github_url: "https://github.com/seovee/masterClass-Fin",
-    skill: ["NextJS", "React", "Framer-motion", "Gsap", "TypeWriter-effect"],
+    desc: "회원가입, 로그인 기능구현 웹페이지입니다.",
+    img: Project_5,
+    url: "https://seovee.github.io/wanted-pre-onboarding-frontend/",
+    github_url: "https://github.com/seovee/wanted-pre-onboarding-frontend",
+    skill: ["React", "React-Router", "Style-Component"],
   },
   {
     num: 6,
     title: "반응형 구현 페이지",
-    category: "React 프로젝트 / 팀프로젝트(5명)",
-    date: "2023. 11. 07. ~ 11. 24.",
-    desc: "리액트, framer-motion, firebase를 이용한 갤러리 웹페이지입니다.",
-    img: Project_3,
-    url: "https://likelion-fe4-rp11.github.io/Asparagus/",
-    github_url: "https://github.com/seovee/masterClass-Fin",
-    skill: ["NextJS", "React", "Framer-motion", "Gsap", "TypeWriter-effect"],
+    category: "개인프로젝트",
+    date: "2023. 08. 21. ~ 08. 24.",
+    desc: `HTML, CSS로만 반응형을 구현한 웹페이지입니다. 데스크톱, 태블릿, 모바일용 3가지 레이아웃을 적용했습니다.`,
+    img: Project_6,
+    url: "http://seovee.github.io/wanted-fe-challenge/",
+    github_url: "https://github.com/seovee/wanted-fe-challenge",
+    skill: ["HTML", "CSS"],
   },
 ];
