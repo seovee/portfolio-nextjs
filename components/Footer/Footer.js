4503eca62e1140961834d75285bc4fa13f653458
seovee <jinseob102@naver.com>
2023-12-24 20:42:59 +0900

    [Design] 페딩값 수정

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="h-20">
-      <div className="flex justify-center items-end pb-5 text-white text-xs bg-emerald-400 w-full h-full">
+      <div className="flex justify-center items-end pb-3 text-white text-xs bg-emerald-400 w-full h-full">
         Copyright 2023. JinSeob Kim. All rights reserved.
       </div>
     </footer>
